18f5a98ee9cb1c59ec9cd4a9554513bc6345b913
Ismayil Mirzayev <ismayil.mirzayev0@gmail.com>
2021-08-30 15:47:17 +0400

    template uploaded

---
+++ resources/views/front/inc/head.blade.php
@@ -0,0 +1,30 @@
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Glint</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="{{ asset('front/css/base.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/css/vendor.css') }}">
+    <link rel="stylesheet" href="{{ asset('front/css/main.css') }}">
+
+    <!-- script
+    ================================================== -->
+    <script src="{{ asset('front/js/modernizr.js') }}"></script>
+    <script src="{{ asset('front/js/pace.min.js') }}"></script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="{{ asset('front/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('front/favicon.ico') }}" type="image/x-icon">
+
+</head>
